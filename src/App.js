5c2a31e5424597c0cd637df03d7401e5b16d405f
Godley <charlotte.godley@hotmail.co.uk>
2018-03-13 19:22:09 +0000

    feat: add code for loading

--- src/App.js
+++ src/App.js
@@ -4,17 +4,41 @@ import './App.css';
 import Files from 'react-files';
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
 
     // This binding is necessary to make `this` work in the callback
-    this.onFilesChange = this.onFilesChange.bind(this);
     this.onFilesError = this.onFilesError.bind(this);
-    this.save = this.save.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
 
     this.state = {value: "blank",
-  version: "1.0.0"};
+    version: "1.0.0"};
+    this.uploadFile = this.uploadFile.bind(this);
+  
+    this.that = this;
+  }
+  
+  uploadFile(event) {
+      let file = event.target.files[0];
+      console.log(that);
+      console.log(file);
+      
+      if (file) {
+        let data = new FormData();
+        data.append('file', file);
+        // axios.post('/files', data)...
+        let reader = new FileReader();
+        var that = this;
+        reader.onload = function() {
+          let contents_array = this.result.split("\n");
+          console.log(contents_array);
+          that.setState({version: contents_array[0],
+            value: contents_array[1]});
+        };
+        reader.readAsText(file);
+        console.log(reader.result);
+      }
   }
 
   
@@ -25,24 +49,16 @@ class App extends Component {
       <table>
         <tbody>
           <tr>
-            <td><div className="files">
-        <Files
-          className='files-dropzone'
-          onChange={this.onFilesChange}
-          onError={this.onFilesError}
-          accepts={['text/plain']}
-          maxFiles={1}
-          maxFileSize={10000000}
-          minFileSize={0}
-          clickable
-        >
-        Load
-        </Files>
-      </div></td>
+            <td>
+            <span>
+      <input type="file"
+      name="myFile"
+      onChange={this.uploadFile} />
+    </span></td>
             <td><a href={data} download>Save</a></td>
           </tr>
           <tr>
-            <td><textarea defaultValue={this.state.value} onChange={this.handleTextChange} />
+            <td><textarea value={this.state.value} onChange={this.handleTextChange} />
             </td>
           </tr>
           
@@ -50,23 +66,10 @@ class App extends Component {
       </table>
     );
   }
+  
 
   handleTextChange(event) {
-
     this.setState({value: event.target.value});
-  }
-
-  save() {
-    let text = this.version + "\n" + this.state.value;
-
-  }
-
-  onFilesChange(files) {
-    console.log(files);
-  }
- 
-  onFilesError(error, file) {
-    console.log('error code ' + error.code + ': ' + error.message)
   }
 }
 
